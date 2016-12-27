01204bae2a8cd9ba44738ae407cf37a0d939d7d7
Giacomo Berardi <berardi@spaziodati.eu>
2016-12-27 20:05:16 +0100

    new version + better test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Testing fast Cython version"
-python setup.py test
+E1=$(python setup.py test)
 mkdir temp
 mv shallowlearn/word2vec_inner* temp/
 touch shallowlearn/word2vec_inner.pyx
 echo "Testing slow Numpy version"
-python setup.py test
+E2=$(python setup.py test)
 mv temp/word2vec_inner* shallowlearn/
 rm -rf temp
+exit $(($E2 + $E1))

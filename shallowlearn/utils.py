26645c0aaeeed248ff9fc709d6a1422e0141d4a4
Giacomo Berardi <berardi@spaziodati.eu>
2016-12-28 01:50:06 +0100

    hash for python3 fix

--- shallowlearn/utils.py
+++ shallowlearn/utils.py
@@ -36,7 +36,7 @@ class HashIter(object):
     def hash(cls, string):
         # Reproduces hash method used in fastText
         h = numpy.uint32(2166136261)
-        for c in bytearray(string):
+        for c in bytearray(str(string).encode('utf-8')):
             h ^= numpy.uint32(c)
             h *= numpy.uint32(16777619)
         return h
